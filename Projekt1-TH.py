64368177485a2a3477b92c5b870cc06ba2257b80
xhorva04 <Tomas@horvath.site>
2021-10-23 19:06:01 +0200

    Overeni uzivatele – DONE

--- Projekt1-TH.py
+++ Projekt1-TH.py
@@ -1,4 +1,33 @@
-# Projekt 01 – Tomáš Horváth
+# Projekt 01 – Tomáš H.
+TEXTS = ['''
+Situated about 10 miles west of Kemmerer, 
+Fossil Butte is a ruggedly impressive 
+topographic feature that rises sharply 
+some 1000 feet above Twin Creek Valley 
+to an elevation of more than 7500 feet 
+above sea level. The butte is located just 
+north of US 30N and the Union Pacific Railroad, 
+which traverse the valley. ''',
+
+'''At the base of Fossil Butte are the bright 
+red, purple, yellow and gray beds of the Wasatch 
+Formation. Eroded portions of these horizontal 
+beds slope gradually upward from the valley floor 
+and steepen abruptly. Overlying them and extending 
+to the top of the butte are the much steeper 
+buff-to-white beds of the Green River Formation, 
+which are about 300 feet thick.''',
+
+'''The monument contains 8198 acres and protects 
+a portion of the largest deposit of freshwater fish 
+fossils in the world. The richest fossil fish deposits 
+are found in multiple limestone layers, which lie some 
+100 feet below the top of the butte. The fossils 
+represent several varieties of perch, as well as 
+other freshwater genera and herring similar to those 
+in modern oceans. Other fish such as paddlefish, 
+garpike and stingray are also present.'''
+]
 
 db_uzivatelu = {
     'bob': '123',
@@ -7,4 +36,18 @@ db_uzivatelu = {
     'liz': 'pass123',
 }
 
-print(db_uzivatelu)
+oddelovac = "_" * 64
+
+uzivatel = input("Zadejte uživatelské jméno: ")
+heslo = input("Zadejte heslo: ")
+
+if db_uzivatelu.get(uzivatel) == heslo:
+    print(oddelovac)
+    print("Vitejte v Aplikaci:", uzivatel)
+    print("K analyze jsou k dispozici 3 texty.")
+    print(oddelovac)
+else:
+    print("Neplatne jmeno nebo heslo. Ukoncuji...")
+    quit()
+
+
